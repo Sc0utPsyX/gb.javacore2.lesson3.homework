1b57103b13aa1389d3ef2a1c93d0ace2fca2520d
Sc0utPsyX <sc0utpsyx@gmail.com>
2022-04-28 23:22:04 +0300

    т.к внизу есть приписка "как можно меньше добавлять всего", то решал все "в лоб"

--- src/main/java/HomeworkApplication.java
+++ src/main/java/HomeworkApplication.java
@@ -1,3 +1,56 @@
+import java.util.*;
+
 public class HomeworkApplication {
+    public static void main(String[] args) {
+        FirstMethod();
+        SecondMethod();
+    }
+
+    private static void FirstMethod() {
+        String[] arr = {"Hello", "Hola", "Hallo", "Barev", "Aloha", "Hej", "Salute", "Labas", "Moien",
+                "Salut", "Salom", "Terve", "Namaste", "Ahoj", "Aloha", "Salute", "Labas", "Konnichiwa"};
+        Set<String> wordsArray = new LinkedHashSet<>(List.of(arr));
+        System.out.println(wordsArray);
+        System.out.println(wordsArray.size());
+    }
+
+    private static void SecondMethod(){
+        HashMap<String, String> tn = new HashMap<>();
+        add(tn,"Ivanov", "+79263242106");
+        add(tn,"Smirnov", "+79263242107");
+        add(tn, "Kyznechov", "+79263242108");
+        add(tn,"Popov", "+79263242109");
+        add(tn,"Vasiliev", "+79263242110");
+        add(tn, "Petrov", "+79263242111");
+        add(tn,"Sokolov", "+79263242112");
+        add(tn,"Mihailov", "+79263242113");
+        add(tn, "Novikov", "+79263242114");
+        add(tn,"Fedorov", "+79263242104");
+        add(tn,"Morozov", "+79263242105");
+        add(tn, "Ivanov", "+79263242103");
+        add(tn, "Vasiliev", "+79263242102");
+        get(tn, "Ivanov");
+        get(tn, "Smirnov");
+        get(tn, "Kyznechov");
+        get(tn, "Popov");
+        get(tn, "Vasiliev");
+        get(tn, "Petrov");
+        get(tn, "Sokolov");
+        get(tn, "Mihailov");
+        get(tn, "Novikov");
+        get(tn, "Fedorov");
+        get(tn, "Morozov");
+
+    }
+
+    private static void add(HashMap<String, String> tn, String name, String phoneNumber){
+        if (tn.containsKey(name)) {
+            tn.put(name, tn.get(name) + ", " + phoneNumber);
+        } else tn.put(name, phoneNumber);
+    }
+
+    private static void get(HashMap<String, String> tn, String name) {
+        System.out.printf("%s = %s\n", name, tn.get(name));
+    }
 
 }
